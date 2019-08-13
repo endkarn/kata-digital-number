393c5d32c321a2ac936f84f1ed13bfb4d726a9a5
endkarn <end_karn@hotmail.com>
2019-08-13 09:48:49 +0700

    Added : implemented test and func SlplitNumber.

--- number/convert.go
+++ number/convert.go
@@ -1,8 +1,22 @@
 package number
 
+import (
+	"strconv"
+	"strings"
+)
+
 func ConvertIntegerToDigitalNumber(num int) string {
+	var result string
 	if num == 1 {
 		return "|\n|"
 	}
-	return ""
+	if num == 2 {
+		return " _\n  |\n -\n|_"
+	}
+	return result
+}
+
+func SplitNumberToStringArray(num int) []string {
+	stringNum := strconv.Itoa(num)
+	return strings.Split(stringNum, "")
 }
--- number/convert_test.go
+++ number/convert_test.go
@@ -10,3 +10,20 @@ func Test_ConvertIntegerToDigitalNumber_By_1_Should_Be_1(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func Test_SplitNumberToStringArray_By_12_Should_Be_1_2(t *testing.T) {
+	expected := []string{"1","2"}
+
+	actual := SplitNumberToStringArray(12)
+
+	assert.Equal(t,expected,actual)
+
+}
+
+func Test_ConvertIntegerToDigitalNumber_By_2_Should_Be_2(t *testing.T) {
+	expected := " _\n  |\n -\n|_"
+
+	actual := ConvertIntegerToDigitalNumber(2)
+
+	assert.Equal(t, expected, actual)
+}
