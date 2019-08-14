e55e1e54220963a80963b59dad04e95f66852aa3
endkarn <end_karn@hotmail.com>
2019-08-14 09:38:14 +0700

    Added : more testcase about rendering lcd set.

--- number/convert_test.go
+++ number/convert_test.go
@@ -3,8 +3,8 @@ package number
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func TestConvertIntegerIntoArrayTextDigiFormat_By_1_Should_Be_1(t *testing.T) {
-	expected := []string{
+func TestConvertIntegerIntoArrayTextDigiFormat_By_1_Should_Be_ArrayString_1(t *testing.T) {
+	expected := [4]string{
 		"|",
 		"|",
 		"|",
@@ -17,21 +17,21 @@ func TestConvertIntegerIntoArrayTextDigiFormat_By_1_Should_Be_1(t *testing.T) {
 }
 
 func TestSplitNumberToArray_By_12_Shoud_Be_1_2(t *testing.T) {
-	expected := []int{1,2}
+	expected := []int{1, 2}
 
 	actual := SplitNumberToArray(12)
 
 	assert.Equal(t, expected, actual)
 }
 
-func TestGetSetOfDigitalNumber_By_1_2_Should_1_2(t *testing.T) {
-	expected := [][]string{
-		[]string{
+func TestGetSetOfDigitalNumber_By_1_2_Should_Be_ArrayString_1_2(t *testing.T) {
+	expected := [][4]string{
+		[4]string{
 			"|",
 			"|",
 			"|",
 			"|",
-	},[]string{
+		}, [4]string{
 			"---",
 			" _|",
 			"|  ",
@@ -39,20 +39,41 @@ func TestGetSetOfDigitalNumber_By_1_2_Should_1_2(t *testing.T) {
 		},
 	}
 
-	actual := GetSetOfDigitalNumber([]int{1,2})
+	actual := GetSetOfDigitalNumber([]int{1, 2})
 
 	assert.Equal(t, expected, actual)
 }
 
-func TestRenderSetOfDigitalNumberToString_By_1_Should_Be_1(t *testing.T) {
+func TestRenderSetOfDigitalNumberToString_By_1_Should_Be_LCD_1(t *testing.T) {
 	expected := "|\t\n|\t\n|\t\n|\t\n"
 
-	actual := RenderSetOfDigitalNumberToString([][]string{
-		[]string{
+	actual := RenderSetOfDigitalNumberToString([][4]string{
+		[4]string{
 			"|",
 			"|",
 			"|",
 			"|",
+		},
+	})
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestRenderSetOfDigitalNumberToString_By_12_Should_Be_LCD_12(t *testing.T) {
+	expected := "|\t---\t\n|\t _|\t\n|\t|  \t\n|\t---\t\n"
+
+	actual := RenderSetOfDigitalNumberToString([][4]string{
+		[4]string{
+			"|",
+			"|",
+			"|",
+			"|",
+		},
+		[4]string{
+			"---",
+			" _|",
+			"|  ",
+			"---",
 		},
 	})
 
