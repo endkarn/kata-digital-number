cbf48632baeddac45332a1c49edfd609391358ad
endkarn <end_karn@hotmail.com>
2019-08-14 09:39:03 +0700

    Added : more numberic in lcd.

--- number/convert.go
+++ number/convert.go
@@ -15,21 +15,21 @@ func RenderLcdNumber(number int) string {
 
 func RenderSetOfDigitalNumberToString(setNumber [][4]string) string {
 	var rendered string
-		//rows loop
-		for row := 0; row < maxDigitalNumberRows; row++ {
-			for _, number := range setNumber {
-				rendered += number[row] + "\t"
-			}
-			rendered += "\n"
+	//rows loop
+	for row := 0; row < maxDigitalNumberRows; row++ {
+		for _, number := range setNumber {
+			rendered += number[row] + "\t"
 		}
+		rendered += "\n"
+	}
 	return rendered
 }
 
 func GetSetOfDigitalNumber(numbers []int) [][4]string {
 	var digiNumberList [][4]string
-	for _ , number := range numbers {
+	for _, number := range numbers {
 		digiNumber := ConvertIntegerIntoArrayTextDigiFormat(number)
-		digiNumberList = append(digiNumberList,digiNumber)
+		digiNumberList = append(digiNumberList, digiNumber)
 	}
 	return digiNumberList
 }
@@ -46,7 +46,13 @@ func SplitNumberToArray(num int) []int {
 }
 
 func ConvertIntegerIntoArrayTextDigiFormat(num int) [4]string {
-	var numberSet [9][4]string
+	var numberSet [10][4]string
+	numberSet[0] = [4]string{
+		"----",
+		"|  |",
+		"|  |",
+		"----",
+	}
 	numberSet[1] = [4]string{
 		"|",
 		"|",
@@ -64,6 +70,42 @@ func ConvertIntegerIntoArrayTextDigiFormat(num int) [4]string {
 		"  |",
 		"--|",
 		"---",
+	}
+	numberSet[4] = [4]string{
+		"|  |",
+		"|  |",
+		"----",
+		"   |",
+	}
+	numberSet[5] = [4]string{
+		"----",
+		"|___",
+		"    |",
+		"----",
+	}
+	numberSet[6] = [4]string{
+		"----",
+		"|   ",
+		"|---",
+		"|___|",
+	}
+	numberSet[7] = [4]string{
+		"----",
+		"   |",
+		"   |",
+		"   |",
+	}
+	numberSet[8] = [4]string{
+		"----",
+		"|  |",
+		"----",
+		"|__|",
+	}
+	numberSet[9] = [4]string{
+		"----",
+		"|  |",
+		"----",
+		"   |",
 	}
 	return numberSet[num]
 }
